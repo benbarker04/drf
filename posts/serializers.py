6ada5ee9b6fc0493a284358d481034c9df972b00
Ben Barker <benbarker074@gmail.com>
2024-06-18 10:58:11 +0000

    add ability to post and post validater

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,33 +1,34 @@
 from rest_framework import serializers
 from posts.models import Post
 
-class  postSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.name')
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ImageField(source='owner.image.profile.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Image size larger then 2MB!'
+                'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
-                'Image width larger then 4096px!'
-            )
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger then 4096px!'
+                'Image width larger than 4096px!'
             )
         return value
 
-def get_is_owner(self, obj):
+    def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
-class Meta:
-    model = Post
-    fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at', 'updated_at', 'title', 'content', 'image', 'image_filter'
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter'
         ]
