686418169621ad360a1377902c487a2cc5c1b453
Ben Barker <benbarker074@gmail.com>
2024-06-20 16:05:52 +0000

    created likeslist and likesdetial view

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Likes
 
@@ -6,5 +7,13 @@ class LikeSerializer(serializers.ModelSerializer):
 
 
     class Meta:
-        model = Like
+        model = Likes
         fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
