f108335caec858ac93d3883b270e875224f61c20
Ben Barker <benbarker074@gmail.com>
2024-06-24 17:33:59 +0000

    added post and profile filters

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,14 +1,16 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from .models import Followers
 
-class LikeSerializer(serializers.ModelSerializer):
+class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-
+    followed_name = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
-        model = Like
-        fields = ['id', 'created_at', 'owner', 'post']
+        model = Followers
+        fields = [
+            'id', 'owner', 'created_at', 'followed', 'followed_name'
+        ]
 
     def create(self, validated_data):
         try:
