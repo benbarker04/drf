7f3bcf9217294c5a12d58cff75668372f61e65ea
Ben Barker <benbarker074@gmail.com>
2024-06-26 13:24:08 +0000

    add logout route

--- drf_api/views.py
+++ drf_api/views.py
@@ -6,6 +6,27 @@ from .settings import (
 )
 
 @api_view()
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,    
+    )
+    return response
 def root_route(request):
     return Response({
         "message": "welcome to my drf API"
